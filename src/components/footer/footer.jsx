3c6927a0d89e991ce616adf2398f3c744b72c3f1
bumble469 <bumble469@gmail.com>
2025-12-17 13:56:18 +0530

    footer ui change

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="max-w-7xl mx-auto
-      border-2 border-gray-800/50 bg-gray-900/40 backdrop-blur-md
-      px-4 sm:px-6 py-3 rounded-xl shadow-lg
-      flex items-center justify-center">
+    <footer
+      className="lg:absolute bottom-4 left-0 w-full
+      border-t border-gray-800/50 bg-gray-900/40 backdrop-blur-md
+      px-4 sm:px-6 py-3
+      flex items-center justify-center z-10"
+    >
       <p className="text-sm text-center">
         © 2025 Alisher Sayed. All Rights Reserved.
       </p>
     </footer>
+
   );
 };
 
